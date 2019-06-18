57fb394834840a2ec40fc1c40e557402f42915cd
Thom <thomkaptein@gmail.com>
2019-06-18 20:06:33 +0200

    add config warning

--- src/Configurations/TokenConfiguration.cs
+++ src/Configurations/TokenConfiguration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using Bot.Structs;
 using Newtonsoft.Json;
 
@@ -25,6 +26,11 @@ namespace Bot.Configurations
                 Config = new ConfigData();
                 var json = JsonConvert.SerializeObject(Config, Formatting.Indented);
                 File.WriteAllText(ConfigFolder + "/" + ConfigFile, json);
+                Console.WriteLine("Config.json added, please add your configuration\n" +
+                    "Bot will exit in 5 seconds.");
+
+                Thread.Sleep(5000);
+                Environment.Exit(0);
             }
             else
             {
